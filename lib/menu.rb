c545dc2194c464f5fc52994b0cdec029f41c7085
Andy <andygnewman@gmail.com>
2014-12-21 13:37:37 +0000

    tests passed for validation of customer entered cost

--- lib/menu.rb
+++ lib/menu.rb
@@ -10,19 +10,22 @@ class Menu
   end
 
   def load_menu 
-    CSV.foreach('menu.csv') do |l| 
-      dish = l[0] ; price = l[1]
-      @menu_list[dish.delete(' ').to_sym] = price.to_f
+    CSV.foreach('menu.csv') do |line| 
+      dish = line[0] ; price = line[1]
+      @menu_list[dish.to_sym] = price.to_f
     end
   end
 
   def validate_dishes(order_dishes)
-    unlisted_items = order_dishes.reject { |d| menu_list.keys.include?(d) }
-    raise "#{unlisted_items} are not on the menu, please reorder" if order_dishes & menu_list.keys != order_dishes
+    unlisted_items = order_dishes.reject { |order_dish| menu_list.keys.include?(order_dish) }
+    raise "#{unlisted_items} are not on the menu, please reorder" if unlisted_items != nil
   end
 
   def validate_cost(order_all)
-    
+    order_all.each { |line| line[:price] = menu_list[line[:dish].to_sym] }
+    order_cost_total = order_all.inject(0) { | memo, item | memo + item[:cost]}
+    menu_order_cost = order_all.inject(0) { | memo, item | memo + (item[:price] * item[:quantity])}
+    order_cost_total == menu_order_cost ? true : false
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -9,15 +9,15 @@ let(:menu){Menu.new}
   end
 
   it 'should raise an error if an item ordered is not on the menu' do
-    expect{menu.validate_dishes([:Banana])}.to raise_error(RuntimeError, "[:Banana] are not on the menu, please reorder")
+    expect{menu.validate_dishes(["Banana"])}.to raise_error(RuntimeError, "[\"Banana\"] are not on the menu, please reorder")
   end
 
-  it 'should raise an error if the total for the ordered items is not correct' do
-    expect{menu.validate_cost([:PadThai, 3, 30])}.to raise_error(RuntimeError, "Payment should be 26.25")
+  it 'should return false if the total for the ordered items is not correct' do
+    expect(menu.validate_cost([{:dish => "Pad Thai", :quantity => 3, :cost => 30}, {:dish => "Red Curry", :quantity => 2, :cost => 15.5}])).to be false
   end
   
   it 'should return true if passed an order where payment is correct' do
-    expect(menu.validate_cost([:PadThai, 3, 26.25])).to be true
+    expect(menu.validate_cost([{:dish => "Pad Thai", :quantity => 3, :cost => 26.25}, :dish => "Red Curry", :quantity => 2, :cost => 14.5])).to be true
   end
 
 end
