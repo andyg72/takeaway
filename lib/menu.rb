aca92510d2942c2643b3cd32e029c3e5bfc5a415
Andy <andygnewman@gmail.com>
2014-12-21 18:13:23 +0000

    refactored methods from menu into order

--- lib/menu.rb
+++ lib/menu.rb
@@ -16,16 +16,4 @@ class Menu
     end
   end
 
-  def validate_dishes(order_dishes)
-    unlisted_items = order_dishes.reject { |order_dish| menu_list.keys.include?(order_dish) }
-    raise "#{unlisted_items} are not on the menu, please reorder" if unlisted_items != nil
-  end
-
-  def validate_cost(order_all)
-    order_all.each { |line| line[:price] = menu_list[line[:dish].to_sym] }
-    order_cost_total = order_all.inject(0) { | memo, item | memo + item[:cost]}
-    menu_order_cost = order_all.inject(0) { | memo, item | memo + (item[:price] * item[:quantity])}
-    order_cost_total == menu_order_cost ? true : false
-  end
-
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -8,16 +8,4 @@ let(:menu){Menu.new}
     expect(menu.menu_list.length).to be > 0
   end
 
-  it 'should raise an error if an item ordered is not on the menu' do
-    expect{menu.validate_dishes(["Banana"])}.to raise_error(RuntimeError, "[\"Banana\"] are not on the menu, please reorder")
-  end
-
-  it 'should return false if the total for the ordered items is not correct' do
-    expect(menu.validate_cost([{:dish => "Pad Thai", :quantity => 3, :cost => 30}, {:dish => "Red Curry", :quantity => 2, :cost => 15.5}])).to be false
-  end
-  
-  it 'should return true if passed an order where payment is correct' do
-    expect(menu.validate_cost([{:dish => "Pad Thai", :quantity => 3, :cost => 26.25}, :dish => "Red Curry", :quantity => 2, :cost => 14.5])).to be true
-  end
-
 end
