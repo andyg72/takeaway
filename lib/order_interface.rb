69ba3221b3e63ab1d19db27acdb753c13fc99096
Andy <andygnewman@gmail.com>
2014-12-22 17:47:55 +0000

    fix items following integration testing through irb

--- lib/order_interface.rb
+++ lib/order_interface.rb
@@ -1,6 +1,6 @@
 class OrderInterface
 
-  attr_reader :customer_order
+  attr_reader :customer_order, :menu_manager
 
   def initialize
     @customer_order = []
@@ -17,15 +17,21 @@ class OrderInterface
   def print_options
     puts '1. View the menu'
     puts '2. Order a dish'
-    puts '3. Confirm your order'
+    puts '3. View your order'
+    puts '4. Delete an item from your order'
+    puts '5. Delete your entire order'
+    puts '6. Confirm your order'
     puts '9. Exit'
   end
 
   def process(selection)
     case selection
-    when '1' then menu.display_menu
+    when '1' then menu_manager.display_menu
     when '2' then capture_order
-    when '3' then confirm_order
+    when '3' then view_order
+    when '4' then delete_item
+    when '5' then delete_entire_order
+    when '6' then confirm_order
     when '9' then exit
     else puts 'Sorry that option doesn\'t exist, please try again'
     end
@@ -38,25 +44,40 @@ class OrderInterface
     quantity = STDIN.gets.chomp
     puts 'How much is that?'
     cost = STDIN.gets.chomp
-    customer_order << {:dish => dish, :quantity => quantity, :cost => cost}
+    customer_order << {:dish => dish, :quantity => quantity.to_i, :cost => cost.to_f}
   end
 
   def confirm_order
+    raise 'You need to add at least one dish to your order' if customer_order.empty?
     validate_dishes 
     validate_cost
     raise_confirmation_text
-    "You should receive an SMS confirming your order and expected delivery time"
+    puts 'You should receive an SMS confirming your order and expected delivery time'
+  end
+
+  def view_order
+    customer_order.each_with_index { |item, i| puts "#{i+1}. #{item[:dish]}, #{item[:quantity]}, #{item[:cost]}" }  
+  end
+
+  def delete_item
+    view_order
+    puts 'Which line item would you like to delete?'
+    customer_order.delete_at(STDIN.gets.chomp.to_i - 1)
+  end
+
+  def delete_entire_order
+    customer_order.clear
   end
 
   def validate_dishes
     ordered_dishes = []
     customer_order.each { |line| ordered_dishes << line[:dish] }
-    unlisted_items = ordered_dishes.reject { | dish | @menu_manager.menu_dishes.include?(dish) }
-    raise 'Sorry at least one item you ordered isn\'t on the menu' if unlisted_items.length > 0
+    unlisted_items = ordered_dishes.reject { | dish | menu_manager.menu_dishes.include?(dish) }
+    raise "Sorry #{unlisted_items} aren't on the menu" if unlisted_items.empty? == false
   end
 
   def validate_cost
-    customer_order.each { |line| line[:price] = @menu_manager.menu[line[:dish].to_sym] }
+    customer_order.each { |line| line[:price] = menu_manager.menu[line[:dish].to_sym] }
     order_cost_total = customer_order.inject(0) { | memo, item | memo + item[:cost]}
     menu_order_cost = customer_order.inject(0) { | memo, item | memo + (item[:price] * item[:quantity])}
     raise 'Sorry the amount you entered is not correct' if order_cost_total != menu_order_cost 
@@ -64,7 +85,7 @@ class OrderInterface
 
   def raise_confirmation_text(content = TextConfirmation)
     text_confirmation = content.new
-    text_confirmtion.send_confirmation
+    text_confirmation.send_confirmation
   end
 
 
