034bd12eb9d47677138b2d257ed53c3c461dfe8b
Andy <andygnewman@gmail.com>
2014-12-22 15:27:42 +0000

    test for API link creation

--- lib/text_confirmation.rb
+++ lib/text_confirmation.rb
@@ -3,10 +3,19 @@ require 'twilio-ruby'
 
 class TextConfirmation
 
-  attr_reader :body
+  ACCOUNT_SID = 'AC14aa4e1c2ecfa3b75d234b1f9e917bbc'
+  AUTH_TOKEN = '64b492080ce7062d9142e3b6a1a77e65'
 
-  def initialize
+  attr_reader :body, :client
+
+  def send_confiration
+    create_client(Twilio::REST::Client)
     create_body
+    send_text
+  end
+
+  def create_client(api)
+    @client = api.new(ACCOUNT_SID, AUTH_TOKEN)
   end
 
   def create_body
@@ -14,12 +23,7 @@ class TextConfirmation
     @body = "Thank you! Your order was placed and will be delivered before #{delivery_time}."
   end
 
-  def send_text_confirmation
-    account_sid = 'AC14aa4e1c2ecfa3b75d234b1f9e917bbc'
-    auth_token = '64b492080ce7062d9142e3b6a1a77e65'
-
-    client = Twilio::REST::Client.new account_sid, auth_token
-
+  def send_text
     client.account.sms.messages.create(
       :from => '+441490266043',
       :to => '+447525460227',
--- spec/text_confirmation_spec.rb
+++ spec/text_confirmation_spec.rb
@@ -1,15 +1,19 @@
 require 'text_confirmation'
 
 describe TextConfirmation do
-
+  
   let(:text_confirmation){TextConfirmation.new}
+  let(:api_double){double :api_double}
+ 
 
-  it 'create a text message for the expected delivery time' do
-    expect(text_confirmation.body).not_to be nil
+  it 'should create a text message for the expected delivery time' do
+    text_confirmation.create_body
+    expect(text_confirmation.body.length).to eq(68)
   end
 
-  it 'should generate a text message to Twilio' do
-    expect(text_confirmation.construct_text)
+  it 'should create a client link with Twilio' do
+    expect(api_double).to receive(:new).with('AC14aa4e1c2ecfa3b75d234b1f9e917bbc','64b492080ce7062d9142e3b6a1a77e65')
+    text_confirmation.create_client(api_double)
   end
 
 end
