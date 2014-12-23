add5c23c7edf30e7481cb4d529f7f4a18fb99af1
Andy <andygnewman@gmail.com>
2014-12-23 22:21:26 +0000

    set-up order_manager class to test new Domain Model

---
+++ lib/order_manager.rb
@@ -0,0 +1,5 @@
+class OrderManager
+
+# expect to move key methods that manage customer order into here from order_interface
+
+end
---
+++ spec/order_manager_spec.rb
@@ -0,0 +1,8 @@
+require 'order_manager'
+
+describe OrderManager do 
+
+
+  # insert tests releveant to order_manager methods in here after transferring from order_interface
+  
+end
